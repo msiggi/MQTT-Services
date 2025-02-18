fc626370f7da1649fa6fddb7732bee6d3f49287a
Marcel Siegel <marcel@siegelnet.de>
2025-02-18 20:58:35 +0100

    Enable MQTT client and update ResponseWorker registration
    Updated `Program.cs` to ensure `ResponseWorker` is correctly registered as a hosted service and added host execution.
    Changed `Enabled` setting in `appsettings.json` for `MqttClientSettings` from `false` to `true`, activating the MQTT client functionality.

--- SampleClientMessaging2/Program.cs
+++ SampleClientMessaging2/Program.cs
@@ -3,10 +3,11 @@ using MqttServices.Core.Services;
 using SampleClientMessaging2;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<ResponseWorker>();
 
 builder.Services.AddSingleton<IMessagingManager, MessagingManager>();
 builder.Services.AddMqttClientService(opts => builder.Configuration.GetSection(nameof(MqttClientSettings)).Bind(opts));
+
+builder.Services.AddHostedService<ResponseWorker>();
 
 var host = builder.Build();
 host.Run();
