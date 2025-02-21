92f8e1c95e7d88cb20d6b958cf228b45d46389f2
Marcel Siegel <marcel@siegelnet.de>
2025-02-21 15:19:56 +0100

    Refactor MQTT client configuration and update dependencies
    - Removed all projects from `Mqtt-Services.slnLaunch`.
    - Updated `MSTest.TestAdapter` and `MSTest.TestFramework` to version `3.8.2` in `MqttServices.CoreTests.csproj`.
    - Removed `MQTT-Services` reference and added `ProjectReference` entries for `MqttServices.Core` and `SampleCommon` in `SampleWorkerService_Client.csproj`.
    - Updated `Worker.cs` to change `using` directives and enhance message handling logic.
    - Modified MQTT client settings in `appsettings.json`, including enabling the client and updating the broker host.
    - Added a new utility class `Utils` with a method `DumpToConsole` for JSON serialization output.

--- Samples/SampleWorkerService_Client/Worker.cs
+++ Samples/SampleWorkerService_Client/Worker.cs
@@ -1,5 +1,8 @@
-using MQTTnet.Client;
+using MQTTnet;
 using MqttServices.Core.Client;
+using SampleCommon;
+using System.Text;
+using System.Text.Json;
 
 namespace SampleWorkerService_Client
 {
@@ -14,11 +17,19 @@ namespace SampleWorkerService_Client
             this.mqttClientService = mqttClientService;
             this.mqttClientService.ClientConnected += MqttClientService_ClientConnected;
             this.mqttClientService.MessageReceived += MqttClientService_MessageReceived;
+
+            this.mqttClientService.Connect().Wait();
         }
 
         private void MqttClientService_MessageReceived(object? sender, MqttApplicationMessageReceivedEventArgs e)
         {
-            
+            if (e.ApplicationMessage.Topic == "testtopic")
+            {
+                var json = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                var recveivedObject = JsonSerializer.Deserialize<List<GuitarPlayer>>(json);
+
+                recveivedObject.DumpToConsole();
+            }
         }
 
         private void MqttClientService_ClientConnected(object? sender, MqttClientConnectedEventArgs e)
@@ -31,7 +42,36 @@ namespace SampleWorkerService_Client
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                mqttClientService.PublishMessage("testtopic", "this is a test-payload");
+
+                var guitarist = new GuitarPlayer
+                {
+                    Name = "Jimi Hendrix",
+                    BirthDate = new DateTime(1942, 11, 27),
+                    OwnedGuitars = new List<Guitar> { new Guitar { Model = "Stratocaster", Brand = "Fender", Color = "Sunburst" } }
+                };
+
+                var guitarists = new List<GuitarPlayer>
+                {
+                    new GuitarPlayer
+                    {
+                        Name = "Jimi Hendrix",
+                        BirthDate = new DateTime(1942, 11, 27),
+                        OwnedGuitars = new List<Guitar> { new Guitar { Model = "Stratocaster", Brand = "Fender", Color = "Sunburst" } }
+                    },
+                    new GuitarPlayer
+                    {
+                        Name = "Eric Clapton",
+                        BirthDate = new DateTime(1945, 3, 30),
+                        OwnedGuitars = new List<Guitar> { new Guitar { Model = "Stratocaster", Brand = "Fender", Color = "Black" } }
+                    },
+                    new GuitarPlayer                     {
+                        Name = "Jimmy Page",
+                        BirthDate = new DateTime(1944, 1, 9),
+                        OwnedGuitars = new List<Guitar> { new Guitar { Model = "Les Paul", Brand = "Gibson", Color = "Sunburst" } }
+                    }
+                };
+
+                await mqttClientService.PublishMessage("testtopic", guitarists);
                 await Task.Delay(1000, stoppingToken);
             }
         }
