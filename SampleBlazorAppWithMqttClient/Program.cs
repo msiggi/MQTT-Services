aa87dea71ca30218b77f83fd6c306732a51ed29c
Siegel, Marcel (IN DE OEC EASO DIGI) <marcel.siegel@innomotics.com>
2025-10-01 15:06:02 +0200

    new Blazor-SampleApp

---
+++ SampleBlazorAppWithMqttClient/Program.cs
@@ -0,0 +1,33 @@
+using MqttServices.Core.Client;
+using SampleBlazorAppWithMqttClient.Components;
+using SampleBlazorAppWithMqttClient.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMqttMessagingService(opts => builder.Configuration.GetSection(nameof(MqttClientSettings)).Bind(opts));
+builder.Services.AddHostedService<TestBackgroundService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ SampleBlazorAppWithMqttClient/Services/TestBackgroundService.cs
@@ -0,0 +1,35 @@
+﻿
+using MqttServices.Core.Services;
+
+namespace SampleBlazorAppWithMqttClient.Services;
+
+public class TestBackgroundService : IHostedService, IDisposable
+{
+    private readonly IMessagingManager messagingManager;
+
+    public TestBackgroundService(IMessagingManager messagingManager)
+    {
+        this.messagingManager = messagingManager;
+        this.messagingManager.RequestReceived += MessagingManager_RequestReceived;
+    }
+
+    private void MessagingManager_RequestReceived(object? sender, MqttServices.Core.Common.Payload e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+    public void Dispose()
+    {
+        this.messagingManager.RequestReceived -= MessagingManager_RequestReceived;
+    }
+
+}
