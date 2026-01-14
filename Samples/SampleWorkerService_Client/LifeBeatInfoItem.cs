17908fe7a52973ad7bcad89eebe84b817c326e1c
Marcel Siegel <marcel.siegel@innomotics.com>
2026-01-14 14:39:50 +0100

    Tests

---
+++ Samples/SampleWorkerService_Client/LifeBeatInfoItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SampleWorkerService_Client
+{
+    internal class LifeBeatInfoItem
+    {
+        public string? Name { get; internal set; }
+        public string Version { get; internal set; }
+        public DateTime Timestamp { get; internal set; }
+        public DateTime StartupDate { get; internal set; }
+        public string RunningHost { get; internal set; }
+    }
+}
--- Samples/SampleWorkerService_Client/WorkerHostedService.cs
+++ Samples/SampleWorkerService_Client/WorkerHostedService.cs
@@ -7,22 +7,52 @@ using System.Threading.Tasks;
 
 namespace SampleWorkerService_Client;
 
-public class WorkerHostedService : IHostedService
+public class WorkerHostedService : IHostedService, IDisposable
 {
     private readonly IMessagingManager messagingManager;
+    private readonly LifeBeatInfoItem lifeBeatInfo;
+    private Timer? _timer;
+    private int _messageCount = 0;
 
     public WorkerHostedService(IMessagingManager messagingManager)
     {
         this.messagingManager = messagingManager;
-    }   
+        lifeBeatInfo = new LifeBeatInfoItem
+        {
+            Name = System.Reflection.Assembly.GetEntryAssembly().GetName().Name,
+            Version = System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString(),
+            Timestamp = DateTime.UtcNow,
+            StartupDate = DateTime.UtcNow,
+            RunningHost = System.Net.Dns.GetHostName(),
+        };
+    }
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        messagingManager.PublishMqttMessage("test/topic", "Hello from WorkerHostedService");
+        // Timer: alle 5 Sekunden ausführen
+        _timer = new Timer(SendMessage, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
         return Task.CompletedTask;
+    }
+
+    private async void SendMessage(object? state)
+    {
+        _messageCount++;
+
+        lifeBeatInfo.Timestamp = DateTime.UtcNow;
+        var message = $"Hello from WorkerHostedService - Message #{_messageCount} at {DateTime.Now:HH:mm:ss}";
+        Console.WriteLine($"Publishing message: {message}");
+        await messagingManager.SendMessage(lifeBeatInfo);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _timer?.Change(Timeout.Infinite, 0);
         return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
     }
 }
