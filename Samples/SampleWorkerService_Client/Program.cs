b58d8dba17b169ade56ad68ea55181609a7d4c0c
Siegel, Marcel <marcel@siegelnet.de>
2026-01-14 13:35:13 +0100

    tests

--- Samples/SampleWorkerService_Client/Program.cs
+++ Samples/SampleWorkerService_Client/Program.cs
@@ -6,8 +6,10 @@ IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         // for MQTT-Client
-        services.AddMqttClientService(opts => context.Configuration.GetSection(nameof(MqttClientSettings)).Bind(opts));
-        services.AddHostedService<Worker>();
+        services.AddMqttMessagingService(opts => context.Configuration.GetSection(nameof(MqttClientSettings)).Bind(opts));
+
+        //services.AddHostedService<Worker>();
+        services.AddHostedService<WorkerHostedService>();
     })
     .Build();
 
---
+++ Samples/SampleWorkerService_Client/WorkerHostedService.cs
@@ -0,0 +1,28 @@
+﻿using MqttServices.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWorkerService_Client;
+
+public class WorkerHostedService : IHostedService
+{
+    private readonly IMessagingManager messagingManager;
+
+    public WorkerHostedService(IMessagingManager messagingManager)
+    {
+        this.messagingManager = messagingManager;
+    }   
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        messagingManager.PublishMqttMessage("test/topic", "Hello from WorkerHostedService");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
